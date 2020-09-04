f06a4203388cabc018558146edad30cb2fb765a0
Vanessa Dreux <vanessa.dreux@gmail.com>
2020-09-04 15:13:24 +0200

    Création classe ExportSymptomDataFromFile.java

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ExportSymptomDataFromFile.java
@@ -0,0 +1,77 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * The Implements class "ExportSymptomDataFromFile" implements the Interface
+ * class "ISymptomExport". It defines the following method :
+ * <ul>
+ * <li>exportSymptoms()</li>
+ * <ul>
+ * 
+ * @author Vanessa DREUX
+ * @version 1.0
+ *
+ */
+public class ExportSymptomDataFromFile implements ISymptomExport {
+	private String filepath;
+
+	/**
+	 * 
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
+	 */
+	public ExportSymptomDataFromFile(String filepath) {
+		this.filepath = filepath;
+	}
+
+	@Override
+	public void exportSymptoms(String FileNameOut) {
+		// Creation of a new instance of object CleanSymptomDataFromFile to retrieve the
+		// listing of all cleaned symptoms
+		CleanSymptomDataFromFile cleanData = new CleanSymptomDataFromFile(filepath);
+		// Creation of "resultsout" object to write in the file
+		FileWriter resultsout = null;
+		try {
+			// Creation of a new instance of "resultsout" object with "FileNameOut"
+			// parameter
+			resultsout = new FileWriter(FileNameOut);
+		} catch (IOException e) {
+			// Prints a stack trace for the "resultsout" object on the standard error output
+			// stream
+			e.printStackTrace();
+		}
+		BufferedWriter out = new BufferedWriter(resultsout);
+		// Creation of "key" object to retrieve symptoms as defined in cleanSymptoms()
+		// method
+		for (Object key : cleanData.cleanSymptoms().entrySet()) {
+			// try statement to define block of code (between try and catch) to be tested
+			// for errors
+			try {
+				/*
+				 * Writting results in the file with specific format as defined in
+				 * cleanSymptoms() method. "System.lineSeparator" method allows carriage return
+				 */
+				out.write(key + System.lineSeparator());
+			} catch (IOException e) {
+				// Prints a stack trace for the "key" object on the standard error output stream
+				e.printStackTrace();
+			}
+		}
+		// try statement to define block of code (between try and catch) to be tested
+		// for errors
+		try {
+			// flush the content of the buffer to the output stream to force write of the
+			// file
+			out.flush();
+			// Close the stream
+			out.close();
+		} catch (IOException e) {
+			// Prints a stack trace for the "resultsout" object on the standard error output
+			// stream
+			e.printStackTrace();
+		}
+	}
+}
