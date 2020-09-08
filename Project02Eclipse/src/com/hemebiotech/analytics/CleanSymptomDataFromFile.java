7e682727861c05b95997e767d413fc5f94fe9777
Vanessa Dreux <vanessa.dreux@gmail.com>
2020-09-08 11:09:45 +0200

    Refactoring classe CleanSymptomDataFromFile

--- Project02Eclipse/src/com/hemebiotech/analytics/CleanSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/CleanSymptomDataFromFile.java
@@ -17,9 +17,9 @@ import java.util.TreeMap;
  */
 
 public class CleanSymptomDataFromFile implements ISymptomCleaner {
-	// Creation of an empty HashMap to store keys
-	Map<String, Integer> hash_map = new HashMap<String, Integer>();
-	// Creation of an empty TreeMap to store couples (key, occurrence) in ascending
+	// Creation of an empty HashMap to store couples (symptom, occurrence)
+	Map<String, Integer> hashMap = new HashMap<String, Integer>();
+	// Creation of an empty TreeMap to store couples (symptom, occurrence) in ascending
 	// order
 	TreeMap<String, Integer> sorted = new TreeMap<>();
 
@@ -39,24 +39,20 @@ public class CleanSymptomDataFromFile implements ISymptomCleaner {
 		// Creation of a new instance of object ReadSymptomDataFromFile to retrieve the
 		// raw listing of all symptoms
 		ReadSymptomDataFromFile readData = new ReadSymptomDataFromFile(filepath);
-		// For each loop defines hash_map keys following conditions
+		// For each loop defines hash_map following conditions
 		for (String symptom : readData.GetSymptoms()) {
-			// Condition if line doesn't contain any symptom so the key would be
-			// {line,constant value = 1}
-			if (!hash_map.containsKey(symptom)) {
-				hash_map.put(symptom, 1);
-				// Otherwise key would be {line, value increased by 1}
+			// Condition if line contains a new symptom so the value would be 1
+			if (!hashMap.containsKey(symptom)) {
+				hashMap.put(symptom, 1);
+				// Otherwise value would be current value increased by 1
 			} else {
-				int newValue = Integer.valueOf(String.valueOf(hash_map.get(symptom)));
-				newValue++;
-				hash_map.put(symptom, newValue);
+				int occurrence = hashMap.get(symptom);
+				occurrence++;
+				hashMap.put(symptom, occurrence);
 			}
 		}
-		// Creation of an "entry" object to sort symptoms in ascending order
-		sorted.putAll(hash_map);
-		for (Map.Entry<String, Integer> entry : sorted.entrySet())
-			// Display results with a specific format {symptoms = number of occurrences}
-			System.out.println(entry.getKey() + " = " + entry.getValue());
+		// Method to copy all of the elements in "HashMap" into TreeMap "sorted" to sort symptoms in ascending order
+		sorted.putAll(hashMap);
 		return sorted;
 	}
 }
