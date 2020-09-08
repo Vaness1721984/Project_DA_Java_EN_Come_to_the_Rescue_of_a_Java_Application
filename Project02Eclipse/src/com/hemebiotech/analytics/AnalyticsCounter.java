c485aa8f18b309c1f2fbe664e0b9d1d8760ed2ad
Vanessa Dreux <vanessa.dreux@gmail.com>
2020-09-08 11:08:34 +0200

    Refactoring classe Main AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,11 +1,11 @@
 package com.hemebiotech.analytics;
 
 /**
- * This class is the entry point into the application.It allows to execute the 2
- * methods "GetSymptoms()" and "exportSymptoms()". It is possible to modify in
- * this class each of the 2 arguments. In the new object "readsymptoms" there is
+ * This class is the entry point into the application.It allows to execute the
+ * method "exportSymptoms()". It is possible to modify in
+ * this class each of the 2 arguments. In the new instance of object "exportsymptoms" there is
  * one argument that contains file "symptoms.txt" to read symptoms. In the new
- * object "exportsymptoms" there is one argument that contains file
+ * method "exportsymptoms()" there is one argument that contains file
  * "results.out.txt" to write results in it.
  * 
  * @author Vanessa DREUX
@@ -17,18 +17,9 @@ public class AnalyticsCounter {
 	 * @param args Java main method accepts a single argument of type String array.
 	 */
 	public static void main(String args[]) {
-		// first get input
-		ReadSymptomDataFromFile readsymptoms = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
-		readsymptoms.GetSymptoms();
-
-		// then rearrange data
-		/*
-		 * CleanSymptomDataFromFile cleanSymptoms = new
-		 * CleanSymptomDataFromFile("Symptoms.txt"); cleansymptoms.clean();
-		 */
-
-		// next generate output
+		// Generate output
 		ExportSymptomDataFromFile exportsymptoms = new ExportSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		exportsymptoms.exportSymptoms("results.out.txt");
+		System.out.println("Export généré!");
 	}
 }
